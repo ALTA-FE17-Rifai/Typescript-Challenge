895b9622ec1265e0d0b86d5ce890eee6b590680d
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-22 18:53:59 +0700

    Problem 4 - Caesar Cipher

---
+++ CaesarCipher.ts
@@ -0,0 +1,8 @@
+function caesarCipher(offset, input) {
+
+    console.log(caesarCipher(3, "abc"))
+    console.log(caesarCipher(2, "alta"))
+    console.log(caesarCipher(10, "alterraacademy"))
+    console.log(caesarCipher(1, "abcdefghijklmnopqrstuvwxyz"))
+
+    console.log(caesarCipher(1000, "abcdefghijklmnopqrstuvwxyz"))
