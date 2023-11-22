140865b1125bc8da78f8bf96a47c788594b463a2
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-22 18:48:50 +0700

    Problem 3 - Find Min and Max Number

---
+++ minandmax.ts
@@ -0,0 +1,17 @@
+function findMindAndMax(arr){
+
+    int a = (5, 7, 4, -2, -1, 8);
+    int b = (2, -5, -4, 22, 7, 7);
+    int c = (4, 3, 9, 4, -21, 7);
+    int d = (-1, 5, 6, 4, 2, 18);
+    int e = (-2, 5, -7, 4, 7, -20);
+
+    console.log(findMindAndMax([5, 7, 4, -2, -1, 8]))
+
+    console.log(findMindAndMax([2, -5, -4, 22, 7, 7]))
+
+    console.log(findMindAndMax([4, 3, 9, 4, -21, 7]))
+
+    console.log(findMindAndMax([-1, 5, 6, 4, 2, 18]))
+
+    console.log(findMindAndMax([-2, 5, -7, 4, 7, -20]))
